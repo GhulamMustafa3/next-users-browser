720750d85798c17dd2b0b93be5dc233e0e83a58c
GhulamMustafa3 <gmustafafaisal658@gmail.com>
2025-08-27 06:49:02 -0700

    Fix img to Image and add Avatar Url Config in NextConfig

--- users-browser/components/Avatar.jsx
+++ users-browser/components/Avatar.jsx
@@ -1,11 +1,13 @@
 "use client";
-
+import Image from 'next/image'
 export default function Avatar({ src }) {
   return (
-    <img
-      src={src || "https://avatar.iran.liara.run/public/34"}
+    <Image
+      src={src }
       alt="User Avatar"
       className="rounded-full w-16 h-16 object-cover"
+      width={100}
+      height={100}
     />
   );
 }
